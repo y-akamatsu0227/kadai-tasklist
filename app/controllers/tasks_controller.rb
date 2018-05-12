8485980457cf708de3b72fb49424d5a96da23cc8
y.akamatsu <git config --global user.email>
2018-05-12 09:56:54 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,6 +50,9 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content , :status)
   end
 end
+
+#タスク投稿できなくなった
+#ステータスフォームが入力できない
