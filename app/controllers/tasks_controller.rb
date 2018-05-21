bea562e6c8e7c3dd7a364e41bd8086ee24687b21
y.akamatsu <git config --global user.email>
2018-05-21 11:46:27 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,19 @@
 class TasksController < ApplicationController
+ before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
   end
   
-  def show 
+  def show
+    @user = current_user
     @task = Task.find(params[:id])
+    counts(@user)
   end
   
   def new
@@ -12,14 +21,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'タスクが正常に投稿されました'
+      flash[:success] = 'タスクを投稿しました。'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスク が投稿されませんでした'
-      render :new
+       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'tasks/index'
     end
   end
   
@@ -40,16 +49,22 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
-    flash[:success] = 'タスク　は正常に削除されました'
+    flash[:success] = 'メッセージを削除しました。'
     redirect_to tasks_url
   end
+
   
   private
   
   def task_params
     params.require(:task).permit(:content , :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 20 }
   validates :status, presence: true, length: { maximum: 20 }
+
+  belongs_to :user
 end
